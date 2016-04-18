7394267fdd708007895ed5cdac8985e7585e793c
sariyasiddiqui <sariyasiddiqui@Sariyas-MacBook-Pro.local>
2016-04-18 18:59:24 -0400

    tasks with dates

---
+++ calendar/imports/ui/body.js
@@ -0,0 +1,11 @@
+import { Template } from 'meteor/templating';
+ 
+import './body.html';
+ 
+Template.body.helpers({
+  events: [
+    { text: 'This is task 1' , time: '5/6'},
+    { text: 'This is task 2' , time: '7/5'},
+    { text: 'This is task 3' , time: '7/5'},
+  ],
+});
