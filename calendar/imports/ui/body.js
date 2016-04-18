bf49ca2effdece4acfecf88a67f80ad84bd854b7
sariyasiddiqui <sariyasiddiqui@Sariyas-MacBook-Pro.local>
2016-04-18 19:03:28 -0400

    it works

--- calendar/imports/ui/body.js
+++ calendar/imports/ui/body.js
@@ -3,7 +3,7 @@ import { Template } from 'meteor/templating';
 import './body.html';
  
 Template.body.helpers({
-  events: [
+  tasks: [
     { text: 'This is task 1' , time: '5/6'},
     { text: 'This is task 2' , time: '7/5'},
     { text: 'This is task 3' , time: '7/5'},
